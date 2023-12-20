8f76fca373012ec0ea63caf5a9f31d7418c1e9c2
stephen <991266+stxphxn@users.noreply.github.com>
2023-12-20 19:38:43 +0000

    Add new gauges ➕

--- src/components/contextual/pages/vebal/LMVoting/testnet-voting-pools.ts
+++ src/components/contextual/pages/vebal/LMVoting/testnet-voting-pools.ts
@@ -194,10 +194,76 @@ export function telosVotingPools(testnet: 'telos'): ApiVotingPool[] {
     },
   };
 
+  const BTCb_ETH_USDC: ApiVotingPool = {
+    chain: testnet as GqlChain,
+    id: '0x0ca5d4b7aeeca61aff78c8f734596ec88456d5c300010000000000000000000a',
+    address: '0x2d714951f7165a51e8bd61f92d8a979ab0ed4b59',
+    type: GqlPoolMinimalType.Weighted,
+    symbol: '40BTCb-40ETH-20USDC',
+    tokens: [
+      {
+        address: '0x7627b27594bc71e6Ab0fCE755aE8931EB1E12DAC',
+        weight: '0.4',
+        symbol: 'BTCb',
+        logoURI:
+          'https://raw.githubusercontent.com/telosnetwork/token-list/main/logos/BTC.b.png',
+      },
+      {
+        address: '0xA0fB8cd450c8Fd3a11901876cD5f17eB47C6bc50',
+        weight: '0.4',
+        symbol: 'ETH',
+        logoURI: 'https://cryptologos.cc/logos/ethereum-eth-logo.png',
+      },
+      {
+        address: '0xF9485b3fffd191e28a089C21cD745cc228a181E3',
+        weight: '0.2',
+        symbol: 'USDC',
+        logoURI:
+          'https://assets-cdn.trustwallet.com/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
+      },
+    ],
+    gauge: {
+      address: '0x760dd84074f735bd3836718c0916a828d1dda502',
+      isKilled: false,
+      relativeWeightCap: '0.2',
+      addedTimestamp: 1703095734,
+    },
+  };
+
+  const USDM_USDC_USDT: ApiVotingPool = {
+    chain: testnet as GqlChain,
+    id: '0x5e99843486cf052baf0925a0cdeb40920477295900000000000000000000000b',
+    address: '0x5E99843486Cf052BaF0925A0CdeB409204772959',
+    type: GqlPoolMinimalType.Stable,
+    symbol: 'USDM/USDC-USDT',
+    tokens: [
+      {
+        address: '0x8f7D64ea96D729EF24a0F30b4526D47b80d877B9',
+        weight: null,
+        symbol: 'USDM',
+        logoURI:
+          'https://raw.githubusercontent.com/swapsicledex/swapsicle-token-list/master/logos/telos/0x8f7D64ea96D729EF24a0F30b4526D47b80d877B9/logo.png',
+      },
+      {
+        address: '0x058D4893eFa235D86CcEeD5a7Eef0809B76c8c66',
+        weight: null,
+        symbol: 'USDC-USDT',
+        logoURI:
+          'https://raw.githubusercontent.com/centfinance/tokenlists/main/src/assets/images/tokens/USDC-USDT.png',
+      },
+    ],
+    gauge: {
+      address: '0x6e0c092b93b5c075239ee61cae811924028899ea',
+      isKilled: false,
+      addedTimestamp: 1703095734,
+      relativeWeightCap: '0.2',
+    },
+  };
+
   const tSYMM_WTLOS: ApiVotingPool = {
     chain: testnet as GqlChain,
     id: '0xbf0fa44e5611c31429188b7dcc59ffe794d1980e000200000000000000000009',
-    address: '0xbf0FA44e5611C31429188B7dcc59ffe794D1980e',
+    address: '0x0CA5D4b7AeEcA61aFF78c8f734596eC88456d5C3',
     type: GqlPoolMinimalType.Weighted,
     symbol: 'S-80TSYMM-20WTLOS',
     tokens: [
@@ -247,5 +313,13 @@ export function telosVotingPools(testnet: 'telos'): ApiVotingPool[] {
     },
   };
 
-  return [tSYMM_WTLOS, STLOS_WTLOS, USDC_USDT, WTLOS_USDT, Team_Allocation];
+  return [
+    tSYMM_WTLOS,
+    STLOS_WTLOS,
+    USDC_USDT,
+    WTLOS_USDT,
+    BTCb_ETH_USDC,
+    USDM_USDC_USDT,
+    Team_Allocation,
+  ];
 }
