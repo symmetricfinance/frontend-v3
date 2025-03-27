8478e074d27fa400d702370cd8e29838db99d4f6
stephen <991266+stxphxn@users.noreply.github.com>
2025-03-28 01:05:45 +0700

    fix: update sdk

--- src/lib/balancer.sdk.ts
+++ src/lib/balancer.sdk.ts
@@ -1,4 +1,4 @@
-import { BalancerSDK } from '@symmetric-v3/sdk-telos2';
+import { BalancerSDK } from '@symmetric-v3/sdk';
 import { Network } from '@/lib/config/types';
 import { configService } from '@/services/config/config.service';
 import { ref } from 'vue';
@@ -39,15 +39,6 @@ import { subgraphFallbackService } from '@/services/balancer/subgraph/subgraph-f
 //   },
 //   pools: {},
 // };
-
-export const telosOld = new BalancerSDK({
-  network: configService.network.chainId as Network,
-  rpcUrl: configService.rpc,
-  customSubgraphUrl: configService.network.subgraph,
-  sor: {
-    tokenPriceService: 'subgraph',
-  },
-});
 
 export const balancer = new BalancerSDK({
   network: configService.network.chainId as Network,
--- src/lib/config/telos/rateProviders.ts
+++ src/lib/config/telos/rateProviders.ts
@@ -4,6 +4,12 @@ const rateProviders: RateProviders = {
   '*': {
     '0x0000000000000000000000000000000000000000': true,
   },
+  '0xb4b01216a5bc8f1c8a33cd990a1239030e60c905': {
+    '0x9a8f4ba7d632e0d510e7982ff5a9c9e898c102b9': true,
+  },
+  '0xfdff55a36f3dd3942a4ac5aebe68972d57296925': {
+    '0x3c8b7581f87cdc3cd5744da4e8fabb78ef7fae3f': true,
+  },
 };
 
 export default rateProviders;
