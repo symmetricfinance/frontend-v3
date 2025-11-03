e0f3b5eecb88094e6b0089726e316e4a1423c885
stephen <991266+stxphxn@users.noreply.github.com>
2025-11-03 22:10:20 +0700

    remove nested pool

--- src/lib/config/meter/pools.ts
+++ src/lib/config/meter/pools.ts
@@ -72,7 +72,7 @@ const pools: Pools = {
     },
   },
   Deep: [
-    '0xd9fe77653c2b75cf3442c365a3f1f9c7ed1612c7000200000000000000000003', //MTRG/USDT-USDC-suUSD
+   //'0xd9fe77653c2b75cf3442c365a3f1f9c7ed1612c7000200000000000000000003', //MTRG/USDT-USDC-suUSD
   ],
   Deprecated: {},
   GaugeMigration: {},
